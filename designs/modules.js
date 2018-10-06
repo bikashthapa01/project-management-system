6b2934e089209c7d54e6a989c414714f9b8a7464
thapabikash48@gmail.com <thapabikash48@gmail.com>
2018-10-06 10:03:04 +0530

    Home Page added

---
+++ designs/modules.js
@@ -0,0 +1,11 @@
+document.querySelector("#msg").innerHTML = "Connected.";
+
+
+
+var btn = document.querySelector("button");
+
+btn.addEventListener("click",function(){
+
+   btn.style.background="red";
+
+});
